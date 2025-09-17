e82693b9aa8d9d91493d8dc462f9a157734b1e65
EngMohamed71 <mf2976527@gmail.com>
2025-09-17 23:46:47 +0300

    ---

---
+++ DepiTasks/4/RunMe.sh
@@ -0,0 +1,6 @@
+kubectl create -f nginxConfigMap.yml
+kubectl create -f nginxPv.yml
+kubectl create -f nginxpvc.yml
+kubectl create -f nginxPod1.yml  
+kubectl create -f nginxPod2.yml
+kubectl create -f nginxService.yml
